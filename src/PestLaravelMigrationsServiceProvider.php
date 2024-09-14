ee8f84e0ecd939c1ae91783f26b703f89b2a920f
Jordan Welch <jordanhwelch@gmail.com>
2024-09-14 13:29:31 -0500

    If not running unit tests do not boot service provider

--- src/PestLaravelMigrationsServiceProvider.php
+++ src/PestLaravelMigrationsServiceProvider.php
@@ -10,6 +10,10 @@ final class PestLaravelMigrationsServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        if (! $this->app->runningUnitTests()) {
+            return;
+        }
+
         $this->app->singleton(
             MigrationTestMigrator::class,
             fn ($app): MigrationTestMigrator => new MigrationTestMigrator($app['migrator'])
