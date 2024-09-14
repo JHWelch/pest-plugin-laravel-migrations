6a27d91d49ac4e132d770b34acf43717d72a3f1c
Jordan Welch <jordanhwelch@gmail.com>
2024-09-14 00:07:26 -0500

    test only `[$up]` migration

--- tests/ExampleUsageTest.php
+++ tests/ExampleUsageTest.php
@@ -8,6 +8,20 @@ use function JHWelch\PestLaravelMigrations\migration;
 uses(TestCase::class);
 
 it('combines first_name and last_name into full_name', function () {
+    [$up] = migration('2024_09_12_000000_update_users_table_combine_names');
+
+    $user = User::create([
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+    ]);
+
+    $up();
+
+    expect($user->fresh())
+        ->full_name->toEqual('John Doe');
+});
+
+it('combines first_name and last_name into full_name and can revert', function () {
     [$up, $down] = migration('2024_09_12_000000_update_users_table_combine_names');
 
     $user = User::create([
